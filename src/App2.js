dd0f9dbb368cca9c9b2906d6a47fa2dd05b18651
“SilogiNandakishorechary <21r01a67j4@cmritonline.ac.in>
2024-07-21 13:39:57 +0530

    inital commit

---
+++ src/App2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App2.css"
+export default function() {
+  return (
+    <div> 
+        <h3 className='App2'>Welcome to mysore</h3>
+        <p className='txtStyle'>ollege in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicer
+        </p>
+    </div>
+  )
+}
